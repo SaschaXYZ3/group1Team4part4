363a14ee554113515a12179ca8f68d41332df525
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2024-06-05 20:39:52 +0200

    commet @Override in Factory

--- src/main/java/at/ac/fhcampuswien/fhmdb/pattern/factory/Factory.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/pattern/factory/Factory.java
@@ -1,18 +1,19 @@
 package at.ac.fhcampuswien.fhmdb.pattern.factory;
 
-import at.ac.fhcampuswien.fhmdb.HomeController;
+import at.ac.fhcampuswien.fhmdb.controllers.MainController;
+import at.ac.fhcampuswien.fhmdb.controllers.MainController;
 
 import javax.security.auth.callback.Callback;
 
-public class Factory implements Callback<Class<?>, Object> {
-    private static HomeController homeController;
-    public HomeController getHomeController(){
+public class Factory {//implements Callback<Class<?>, Object> {
+    private static MainController homeController;
+    public MainController getHomeController(){
         if (homeController == null) {
-            homeController = new HomeController();
+            homeController = new MainController();
         }
         return homeController;
     }
-    @Override
+    //@Override
     public Object call(Class<?> aClass) {
         try {
             return getHomeController();
